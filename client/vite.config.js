680bac923417e54a3abf2febe9bccc048f23ed6f
ipunzo2026 <ivanpunzocruz@gmail.com>
2026-07-09 17:17:38 -0600

    Revert "Test ngrok"
    This reverts commit 3911859da186458144787ea31130ce4ebd10132a.

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-            target: 'https://plunging-krypton-transform.ngrok-free.dev',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
