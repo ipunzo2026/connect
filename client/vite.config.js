3911859da186458144787ea31130ce4ebd10132a
ipunzo2026 <ivanpunzocruz@gmail.com>
2026-07-09 17:14:59 -0600

    Test ngrok

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+            target: 'https://plunging-krypton-transform.ngrok-free.dev',
         changeOrigin: true,
         secure: false,
       },
