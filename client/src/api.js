1319cb413943cf71f14363f7a677a73b4dfe4ba8
ipunzo2026 <ivanpunzocruz@gmail.com>
2026-07-09 18:04:42 -0600

    Fix api

--- client/src/api.js
+++ client/src/api.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://plunging-krypton-transform.ngrok-free.dev'
+    // Le agregamos el /api directamente aquí al final
+    baseURL: 'https://plunging-krypton-transform.ngrok-free.dev/api'
 });
 
-// Esto soluciona los archivos que usan: import api from './api'
 export default api;
-
-// Esto soluciona los archivos que usan: import { api } from './api'
 export { api };
