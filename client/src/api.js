28f549eff793d878e20ce4c75b1cb8e9ff5fe785
ipunzo2026 <ivanpunzocruz@gmail.com>
2026-07-09 18:52:02 -0600

    Fix: agregar cabecera para saltar advertencia de ngrok

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,12 @@ import axios from 'axios';
 
 const api = axios.create({
     // Le agregamos el /api directamente aquí al final
-    baseURL: 'https://plunging-krypton-transform.ngrok-free.dev/api'
+    baseURL: 'https://plunging-krypton-transform.ngrok-free.dev/api',
+    headers: {
+        //Se salta la pantalla de advertencia de Ngrok por completo
+        'ngrok-skip-browser-warning': 'true'
+    }
+
 });
 
 export default api;
